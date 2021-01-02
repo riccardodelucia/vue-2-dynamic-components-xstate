32853e3c5ba6d5891c5ed93851b33a6114965ad6
riccardodelucia <riccardoroberto.delucia@gmail.com>
2021-01-02 15:48:21 +0100

    Added static content

--- src/components/NumberedStates.vue
+++ src/components/NumberedStates.vue
@@ -1,9 +1,23 @@
 <template>
-  <button>Test</button>
+  <div>
+    <button>Test</button>
+    <div class="page-container">
+      <h1 class="text">Test</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.page-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+  margin: 50px;
+  background-color: yellowgreen;
+}
+</style>
